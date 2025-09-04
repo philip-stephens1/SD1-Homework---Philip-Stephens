bb2e1a85242757df9cb6b844e7974b28f64b1855
philip-stephens1 <philip.stephens1@marist.edu>
2025-09-04 13:25:37 -0400

    Update homework.java

--- Assignment 1 /homework.java
+++ Assignment 1 /homework.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("How old are you? ");
+
+        String age = sc.nextLine();
+        System.out.println("You are " + age + " years old!");
+
+        int num1 = 10;
+        int num2 = 37;
+        int sum = num1+num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum + ".");
+
+
+    }
+}
